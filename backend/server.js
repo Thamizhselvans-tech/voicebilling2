4578836c63efc82c9853fd13d2c3c0290f2c072a
Thamizhselvan <s.thamizhselvan911@gmail.com>
2026-04-09 12:26:42 +0530

    fix cors final

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
